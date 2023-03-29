3415f178a9efa7c1835cca45b1e2c5f8fcd8a881
Andrei Ferestraoaru <andrei.ferestraoaru@sap.com>
2023-03-29 21:40:20 +0300

    Start Project

---
+++ src/main/java/com/trafic/status/traficStatus/TraficStatusApplication.java
@@ -0,0 +1,13 @@
+package com.trafic.status.traficStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraficStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraficStatusApplication.class, args);
+	}
+
+}
