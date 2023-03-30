cda0c805c1df03d100449bf3df6f591f541c870c
Andrei Ferestraoaru <andrei.ferestraoaru@sap.com>
2023-03-30 19:53:38 +0300

    work in progress to connect DB locally. entityManagerFactory cached

---
+++ src/main/java/com/trafficstatus/trafficstatus/DAO/UserRepository.java
@@ -0,0 +1,9 @@
+package com.trafficstatus.trafficstatus.DAO;
+
+import com.trafficstatus.trafficstatus.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
--- src/main/java/com/trafficstatus/trafficstatus/controller/ContentController.java
+++ src/main/java/com/trafficstatus/trafficstatus/controller/ContentController.java
@@ -1,9 +1,10 @@
 package com.trafficstatus.trafficstatus.controller;
 
+import com.trafficstatus.trafficstatus.DAO.UserRepository;
 import com.trafficstatus.trafficstatus.DTO.HelloWsDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.trafficstatus.trafficstatus.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -13,8 +14,28 @@ public class ContentController {
     private static final String contentTemplate = "Hello, tarane! %s" ;
     private final AtomicLong contentCounter = new AtomicLong();
 
+    private UserRepository userRepository;
     @GetMapping("/salut")
     public HelloWsDTO helloCall(@RequestParam(value = "name", defaultValue = "Merge Treaba?") String name){
         return new HelloWsDTO(String.format(contentTemplate, name), (contentCounter.incrementAndGet()));
+    }
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser (@RequestParam String name
+            , @RequestParam String email) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
     }
 }
